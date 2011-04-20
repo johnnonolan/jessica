a03279d5f1c8bc2d0c828a41988c25cfe207946d
Tom Bell <tomb@tombell.org.uk>
2011-04-20 15:34:53 +0100

    Updated Response with some static helper methods, and related unit tests

--- src/Jessica.Tests/Responses/ResponseTests.cs
+++ src/Jessica.Tests/Responses/ResponseTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Jessica.Configuration;
 using Jessica.Responses;
 using NUnit.Framework;
 
@@ -7,6 +8,18 @@ namespace Jessica.Tests.Responses
     [TestFixture]
     public class ResponseTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            Jess.Configuration = new JessicaConfiguration();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Jess.Configuration = null;
+        }
+
         [Test]
         public void Constructor_ShouldReturnAResponseWithStatusCodeOk()
         {
@@ -65,6 +78,62 @@ namespace Jessica.Tests.Responses
             var contents = reader.ReadToEnd();
 
             Assert.That(contents, Is.EqualTo("Hello, world!"));
+        }
+
+        [Test]
+        public void AsFile_WithFilePath_ShouldReturnResponseWithCorrectContentType()
+        {
+            var response = Response.AsFile("../../../Fakes/Files/HelloWorld.txt");
+
+            Assert.That(response.ContentType, Is.EqualTo("application/octet-stream"));
+        }
+
+        [Test]
+        public void AsJson_WithObject_ShouldReturnResponseWithCorrectContentType()
+        {
+            var response = Response.AsJson("Hello, world");
+
+            Assert.That(response.ContentType, Is.EqualTo("application/json"));
+        }
+
+        [Test]
+        public void AsRedirect_WithLocation_ShouldReturnResponseWithCorrectHeader()
+        {
+            var response = Response.AsRedirect("http://google.com");
+
+            Assert.That(response.Headers["Location"], Is.EqualTo("http://google.com"));
+        }
+
+        [Test]
+        public void AsHtml_WithFilePath_ShouldReturnResponseWithCorrectContenType()
+        {
+            var response = Response.AsHtml("../../../Fakes/Files/FakeHtml.html");
+
+            Assert.That(response.ContentType, Is.EqualTo("text/html"));
+        }
+
+        [Test]
+        public void AsCss_WithFilePath_ShouldReturnResponseWithCorrectContentType()
+        {
+            var response = Response.AsCss("../../../Fakes/Files/Base.css");
+
+            Assert.That(response.ContentType, Is.EqualTo("text/css"));
+        }
+
+        [Test]
+        public void AsJs_WithFilePath_ShouldReturnResponseWithCorrectContentType()
+        {
+            var response = Response.AsJs("../../../Fakes/Files/SimpleJs.js");
+
+            Assert.That(response.ContentType, Is.EqualTo("text/javascript"));
+        }
+
+        [Test]
+        public void AsText_WithStringFormat_ShouldReturnResponseWithCorrectContentType()
+        {
+            var response = Response.AsText("Hello, {0}", "world!");
+
+            Assert.That(response.ContentType, Is.EqualTo("text/plain"));
         }
     }
 }
